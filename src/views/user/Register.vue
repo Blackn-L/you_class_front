94bd800e27eaf8be2f4f68f505497abf2be655e1
Blackn <nilinfeng6318@gmail.com>
2019-01-30 23:17:08 +0800

    修改register数据结构

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -2,79 +2,79 @@
   <div id="register">
     <div class="row-header">
       <cube-input
-        v-model="email"
+        v-model="formItem.email"
         placeholder="请输入邮箱"
       ></cube-input>
       <cube-validator
-        v-model="emailValidator"
-        :model="email"
-        :rules="emailRule"
-        :messages="emailMsg"
+        v-model="validators.emailValidator"
+        :model="formItem.email"
+        :rules="validateRules.emailRule"
+        :messages="errorMsgs.emailMsg"
       >
       </cube-validator>
     </div>
     <div class="row">
       <cube-input
-        v-model="username"
+        v-model="formItem.username"
         placeholder="请输入用户名"
       ></cube-input>
       <cube-validator
-        v-model="usernameValidator"
-        :model="username"
-        :rules="usernameRule"
-        :messages="usernameMsg"
+        v-model="validators.usernameValidator"
+        :model="formItem.username"
+        :rules="validateRules.usernameRule"
+        :messages="errorMsgs.usernameMsg"
       >
       </cube-validator>
     </div>
     <div class="row">
       <cube-input
-        v-model="password"
+        v-model="formItem.password"
         placeholder="请输入密码"
       ></cube-input>
       <cube-validator
-        v-model="passwordValidator"
-        :model="password"
-        :rules="passwordRule"
-        :messages="passwordMsg"
+        v-model="validators.passwordValidator"
+        :model="formItem.password"
+        :rules="validateRules.passwordRule"
+        :messages="errorMsgs.passwordMsg"
       >
       </cube-validator>
     </div>
     <div class="row">
       <cube-input
-        v-model="rPassword"
+        v-model="formItem.rPassword"
         placeholder="请再次输入密码"
       ></cube-input>
       <cube-validator
-        v-model="rPasswordValidator"
-        :model="rPassword"
-        :rules="rPasswordRule"
-        :messages="rPasswordMsg"
+        v-model="validators.rPasswordValidator"
+        :model="formItem.rPassword"
+        :rules="validateRules.rPasswordRule"
+        :messages="errorMsgs.rPasswordMsg"
       >
       </cube-validator>
     </div>
     <div class="row">
       <cube-input
-        v-model="code"
+        v-model="formItem.code"
         placeholder="请输入验证码"
       ></cube-input>
       <cube-validator
-        v-model="codeValidator"
-        :model="code"
-        :rules="codeRule"
-        :messages="codeMsg"
+        v-model="validators.codeValidator"
+        :model="formItem.code"
+        :rules="validateRules.codeRule"
+        :messages="errorMsgs.codeMsg"
       >
       </cube-validator>
     </div>
     <div class="row">
       <cube-input
-        v-model="emailCode"
+        v-model="formItem.emailCode"
         placeholder="请输入邮箱验证码"
       ></cube-input>
       <cube-validator
-        v-model="emailCodeValidator"
-        :model="emailCode"
-        :rules="emailCodeRule"
-        :messages="emailCodeMsg"
+        v-model="validators.emailCodeValidator"
+        :model="formItem.emailCode"
+        :rules="validateRules.emailCodeRule"
+        :messages="errorMsgs.emailCodeMsg"
       >
       </cube-validator>
     </div>
@@ -88,93 +88,93 @@ export default {
   name: 'register',
   data() {
     return {
-      email: '', // 邮箱
-      validators: [undefined, undefined, undefined, undefined], // 校验结果数组
-      emailValidator: undefined, // 邮箱校验结果
-      usernameValidator: undefined,
-      passwordValidator: undefined,
-      rPasswordValidator: undefined,
-      codeValidator: undefined, // 图形验证码校验结果
-      emailCodeValidator: undefined, // 邮箱验证码校验结果
-      emailRule: {
-        type: 'email',
-        required: true,
-        notWhitespace: true,
+      formItem: {
+        email: '', // 邮箱
+        username: '', // 用户名
+        password: '', // 密码
+        rPassword: '', // 重复输入密码
+        code: '', // 验证码
+        emailCode: '', // 邮箱验证码
       },
-      usernameRule: {
-        required: true,
-        min: 6,
-        max: 25,
-        notWhitespace: true,
-        pattern: /^[\u4E00-\u9FA5A-Za-z0-9_]+$/,
+      // 验证结果
+      validators: {
+        emailValidator: undefined,
+        usernameValidator: undefined,
+        passwordValidator: undefined,
+        rPasswordValidator: undefined,
+        codeValidator: undefined, // 图形验证码
+        emailCodeValidator: undefined, // 邮箱验证码
       },
-      passwordRule: {
-        required: true,
-        min: 8,
-        max: 25,
-        notWhitespace: true,
-        pattern: /^(?=.*\d)(?=.*[a-zA-Z]).{8,25}$/,
-      },
-      rPasswordRule: {
-        required: true,
-        equal: val => val === this.password,
-      },
-      codeRule: {
-        required: true,
-        min: 4,
-        max: 7,
-      },
-      emailCodeRule: {
-        required: true,
-        min: 4,
-        max: 7, // 预先判断小于4大于7的都是错误的，防止不必要的访问开销
+      // 验证规则
+      validateRules: {
+        emailRule: {
+          type: 'email',
+          required: true,
+          notWhitespace: true,
+        },
+        usernameRule: {
+          required: true,
+          min: 6,
+          max: 25,
+          notWhitespace: true,
+          pattern: /^[\u4E00-\u9FA5A-Za-z0-9_]+$/,
+        },
+        passwordRule: {
+          required: true,
+          min: 8,
+          max: 25,
+          notWhitespace: true,
+          pattern: /^(?=.*\d)(?=.*[a-zA-Z]).{8,25}$/,
+        },
+        rPasswordRule: {
+          required: true,
+          equal: val => val === this.password,
+        },
+        codeRule: {
+          required: true,
+          min: 4,
+          max: 7,
+        },
+        emailCodeRule: {
+          required: true,
+          min: 4,
+          max: 7, // 预先判断小于4大于7的都是错误的，防止不必要的访问开销
+        },
       },
       // 自定义消息提示
-      emailMsg: {
-        required: '邮箱不能为空！',
+      errorMsgs: {
+        emailMsg: {
+          required: '邮箱不能为空！',
+        },
+        usernameMsg: {
+          required: '用户名不能为空',
+          min: '用户名至少为6位',
+          max: '用户名不能超过25位',
+          pattern: '只能输入中文，英文，数字以及下划线',
+        },
+        passwordMsg: {
+          required: '密码不能为空',
+          min: '密码至少为8位',
+          max: '密码不能超过25位',
+          pattern: '密码必须包含字母和数字，不能使用特殊字符',
+        },
+        rPasswordMsg: {
+          required: '二次密码不能为空',
+          equal: '两次密码不一致',
+        },
+        codeMsg: {
+          required: '图形验证码不能为空',
+          isFalse: '图形验证码错误',
+          min: '图形验证码错误',
+          max: '图形验证码错误',
+        },
+        emailCodeMsg: {
+          required: '邮箱验证码不能为空',
+          isFalse: '邮箱验证码错误',
+          min: '邮箱验证码错误',
+          max: '邮箱验证码错误',
+        },
       },
-      usernameMsg: {
-        required: '用户名不能为空',
-        min: '用户名至少为6位',
-        max: '用户名不能超过25位',
-        pattern: '只能输入中文，英文，数字以及下划线',
-      },
-      passwordMsg: {
-        required: '密码不能为空',
-        min: '密码至少为8位',
-        max: '密码不能超过25位',
-        pattern: '密码必须包含字母和数字，不能使用特殊字符',
-      },
-      rPasswordMsg: {
-        required: '二次密码不能为空',
-        equal: '两次密码不一致',
-      },
-      codeMsg: {
-        required: '图形验证码不能为空',
-        isFalse: '图形验证码错误',
-        min: '图形验证码错误',
-        max: '图形验证码错误',
-      },
-      emailCodeMsg: {
-        required: '邮箱验证码不能为空',
-        isFalse: '邮箱验证码错误',
-        min: '邮箱验证码错误',
-        max: '邮箱验证码错误',
-      },
-      emailError: '', // 邮箱错误提示信息
-      emailTrue: false, // 邮箱正确
-      username: '', // 用户名
-      usernameError: '', // 用户名错误提示信息
-      usernameTrue: false,
-      password: '', // 密码
-      passwordError: '', // 密码错误提示信息
-      passwordTrue: false,
-      rPassword: '', // 重复输入密码
-      rPasswordError: '',
-      rPasswordTrue: false,
-      code: '', // 验证码
-      emailCode: '', // 邮箱验证码
-      submitFlag: false, // 是否是提交时执行的
       emailCodeLoding: false, // 邮箱验证码按钮是否显示loading
       emailButtonName: '发送验证码', // 邮箱验证码发送按钮名称
     };
@@ -182,13 +182,6 @@ export default {
   methods: {
     // 提交
     submit() {
-      this.submitFlag = true;
-      this.checkForm();
-      if (!(this.emailTrue && this.usernameTrue && this.passwordTrue && this.rPasswordTrue)) {
-        this.$toast('klll');
-        return false;
-      }
-      this.$toast('交');
       this.removeSpaces();
       // 判断验证码是否正确,这里先不写
       const body = {
@@ -228,13 +221,17 @@ export default {
       this.emailCodeLoding = true;
       api.sendEmailCode().then(({ data }) => {
         if (data.Code === 200) {
-          this.$notify({
-            message: '发送成功',
-            duration: 1000,
-            background: '#1989fa',
+          const toast = this.$createToast({
+            txt: '发送成功',
+            type: 'correct',
           });
+          toast.show();
         } else {
-          this.$notify(data.Msg);
+            const toast = this.$createToast({
+                txt: '发送失败',
+                type: 'error',
+            });
+            toast.show();
         }
       });
       this.emailCodeLoding = false;
