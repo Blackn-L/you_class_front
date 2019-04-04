5a2f0734a312169133a803cc84cf68e30d00c473
Blackn <nilinfeng6318@gmail.com>
2019-04-04 23:44:19 +0800

    课程表布局重新跳转

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,75 +4,50 @@
       slot="content"
       id="main"
     >
-      <el-row style="font-size:16px; margin-top: 10px;">
-        <el-col
-          :span="2"
-          style="margin: 5px 3px;"
-        >3月</el-col>
+      <el-row
+        type='flex'
+        align="middle"
+        justify="space-between"
+      >
+        <el-col :span="2">3月</el-col>
         <el-col :span="21">
           <el-row>
             <el-col
               :span="4"
-              style="background-color: DarkGray; margin: 5px 0px;"
+              style="background-color: DarkGray;"
               v-for="day in days"
               :key="day.key"
             >{{day.value}}</el-col>
           </el-row>
         </el-col>
       </el-row>
-      <el-row
-        style="font-size:13px; margin-top: 10px;"
-        v-for="(values, key, index) in classLists"
-        :key=index
+      <div
+        class="rows"
+        v-for="item in list"
+        :key="item"
       >
-        <div style="
-              width:800px;height:1px;margin:0px
-              auto;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
-        <el-col
-          :span="2"
-          v-if="index === 0"
-          style="margin: 5px 3px;"
-        >一<br><br>08:00<br>┆<br>09:50</el-col>
-        <el-col
-          :span="2"
-          v-if="index === 1"
-          style=" margin: 5px 3px;"
-        >二<br><br>10:10<br>┆<br>12:00</el-col>
-        <el-col
-          :span="2"
-          v-if="index === 2"
-          style=" margin: 5px 3px;"
-        >三<br><br>13:30<br>┆<br>15:20</el-col>
-        <el-col
-          :span="2"
-          v-if="index === 3"
-          style=" margin: 5px 3px;"
-        >四<br><br>15:30<br>┆<br>17:20</el-col>
-        <el-col
-          :span="2"
-          v-if="index === 4"
-          style=" margin: 5px 3px;"
-        >五<br><br>18:30<br>┆<br>20:20</el-col>
-        <el-col :span="21">
-          <el-row>
-            <el-col
-              :span="4"
-              v-for="(value, index) in values"
-              :key="index"
-              style=" margin: 5px 1px;"
-            >
-              <span style="max-width:100px;">
-                <div
-                  v-html="value[0]+'<br><br>'+value[1]"
-                  v-if="value[1]"
-                  inline
-                >
-                </div>
-              </span>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+        <div class="line"></div>
+        <el-row
+          class="inRow"
+          type='flex'
+          align="middle"
+          justify="space-between"
+        >
+          <el-col :span="2">{{ item.label }}</el-col>
+          <el-col
+            :span="3"
+            v-for="(day, index) in classLists"
+            :key="index"
+          >
+            <p class="courseName">{{day[item.session][0]}}</p>
+            <br>
+            <span
+              v-if="day[item.session][1]"
+              class="classroom"
+            >{{day[item.session][1]}}</span>
+          </el-col>
+        </el-row>
+      </div>
     </div>
   </page>
 </template>
@@ -89,8 +64,29 @@ export default {
 
   data() {
     return {
+      list: [
+        {
+          session: 'First',
+          label: '①',
+        },
+        {
+          session: 'Second',
+          label: '②',
+        },
+        {
+          session: 'Third',
+          label: '③',
+        },
+        {
+          session: 'Fourth',
+          label: '④',
+        },
+        {
+          session: 'Fifth',
+          label: '⑤',
+        },
+      ],
       classLists: [], // 课程表信息
-      leftIndex: [1, 2, 3, 4, 5],
       days: [
         {
           key: 'MON',
@@ -135,10 +131,14 @@ export default {
         yearId: '36',
         termId: '2',
       };
+
       api.getClassList(body).then(({ data }) => {
         if (data.Code === 200) {
           console.log(data.Data);
           this.classLists = data.Data;
+        //   api.getMark(body).then((res) => {
+        //     console.log(res.data);
+        //   });
         } else {
           this.$createToast({
             type: 'warn',
@@ -167,8 +167,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less" scoped>
 #main {
   font-size: 16px;
+}
+.line {
+  width: 100%;
+  height: 1px;
+  margin: 0px auto;
+  padding: 0px;
+  background-color: #d5d5d5;
+  overflow: hidden;
+}
+.rows {
+  margin: 2px 5px;
+  .inRow {
+    margin: 7px 5px 2px 2px;
+    font-size: 14px;
+    .courseName {
+      color: black;
+    }
+    .classroom {
+      color: #0c5586;
+    }
+  }
 }
 </style>
