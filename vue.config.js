1f14af6d25fb593631f6733a059a85dede09d486
Blackn <nilinfeng6318@gmail.com>
2019-04-09 18:37:23 +0800

    修改proxy

--- vue.config.js
+++ vue.config.js
@@ -13,22 +13,26 @@ module.exports = {
   lintOnSave: true,
   configureWebpack: {
     module: {
-      rules: [{
-        test: /\.js$/,
-        // 把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
-        loader: 'happypack/loader?id=happyBabel',
-        // 排除node_modules 目录下的文件
-        exclude: /node_modules/,
-      }],
+      rules: [
+        {
+          test: /\.js$/,
+          // 把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
+          loader: 'happypack/loader?id=happyBabel',
+          // 排除node_modules 目录下的文件
+          exclude: /node_modules/,
+        },
+      ],
     },
     plugins: [
       new HappyPack({
         // 用id来标识 happypack处理那里类文件
         id: 'happyBabel',
         // 如何处理  用法和loader 的配置一样
-        loaders: [{
-          loader: 'babel-loader?cacheDirectory=true',
-        }],
+        loaders: [
+          {
+            loader: 'babel-loader?cacheDirectory=true',
+          },
+        ],
         // 共享进程池
         threadPool: happyThreadPool,
         // 允许 HappyPack 输出日志
@@ -44,8 +48,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://jw.tt',
-        // target: 'http://class.blackn.xyz',
+        target: 'http://api.svip52.com/index.php',
+        // target: 'http://jw.tt', // 本地
         ws: true,
         changeOrigin: true,
       },
